4138478a1985d33cde6f9dd1be3883181a5e64cf
ShanmukB <shanmukb@ggktech.com>
2020-09-03 21:54:45 +0530

    Updates

---
+++ precache-manifest.04e02c7db1b14e6fb15bf4a97810bc1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d7e52a0ee9b725a55a49978b474adc",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "ec312271d46e6035d36a",
+    "url": "/Todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d03cb0f907e8b5e32078",
+    "url": "/Todo/static/js/2.815f94b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.815f94b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec312271d46e6035d36a",
+    "url": "/Todo/static/js/main.26a93b3d.chunk.js"
+  },
+  {
+    "revision": "40b31628a98602283aff",
+    "url": "/Todo/static/js/runtime-main.4c66f946.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
